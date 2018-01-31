210488510576fd1c5eefb23326b23f8c1f77c7a9
guiraamos <gui_ramos@live.com>
2018-01-31 09:52:30 -0200

    ..
    ..

--- TP11/Sistema/Analizador.js
+++ TP11/Sistema/Analizador.js
@@ -18,6 +18,8 @@ function Arquivo(modulo, codigo, diretorio, linhas){
 	this.Chamadas = 0;
 	this.Metodos = 0;
 	this.Variaveis = 0;
+	this.Xnove = 0;
+	this.Quadrados = 0;
 }
 
 function ContarLinhasArquivos(arquivo){
@@ -81,22 +83,70 @@ const inicializaPaths = code => {
 		leave: function () {}
 		});
 		
-		console.log("Arquivo: " + arquivo.Diretorio);
-		console.log("Variaveis: " + variaveis);
-		console.log("Chamadas: " + chamadas);
-		console.log("Metodos: " + metodos);
-		console.log("Linhas: " + arquivo.Linhas);
-		console.log("X9: "  + ((variaveis + arquivo.Linhas)/metodos)+chamadas +"\n");
-		
+		if(metodos == 0)
+			metodos = 1;
 		
 		arquivo.Metodos = metodos;
 		arquivo.Variaveis = variaveis;
 		arquivo.Chamadas = chamadas;
+		arquivo.Xnove = ((variaveis + arquivo.Linhas)/metodos)+chamadas;
 	});
-};
+	
+	var menor = 0;
+	
+	//Pega o menor x9
+	for(i=0; i < arquivos.length; i++)
+	{
+		if(arquivos[i+1])
+		{
+			if(arquivos[i].Xnove < arquivos[i+1].Xnove){
+				menor = i;
+			}
+		}
+	}
+	
+	var somaQuadrados = 0;
+	
+	for(i=0; i < arquivos.length; i++)
+	{
+		arquivos[i].Quadrados = Math.round(Math.sqrt(arquivos[i].Xnove/arquivos[menor].Xnove));
+		
+		console.log("Arquivo: "   + arquivos[i].Diretorio);
+		console.log("Variaveis: " + arquivos[i].Variaveis);
+		console.log("Chamadas: "  + arquivos[i].Chamadas);
+		console.log("Metodos: "   + arquivos[i].Metodos);
+		console.log("Linhas: "    + arquivos[i].Linhas);
+		console.log("Quadrados: " + arquivos[i].Quadrados);
+		console.log("X9: "        + arquivos[i].Xnove + "\n");
+		
+		somaQuadrados += arquivos[i].Quadrados;
+	}
+	
+	
+	var quadradosPercorridos = 0;
+	var texto = "{\"key\":\"G1\", \"isGroup\":true, \"pos\":\"0 0\", \"size\":\""+ (somaQuadrados*50)+ " " + (somaQuadrados*50) + "\"} ,\n";
+					
+	for(i=0; i < arquivos.length; i++)
+	{
+		texto += "{\"key\":\""+ arquivos[i].Diretorio +" X9= " + arquivos[i].Xnove + "\", \"color\":\""+gera_cor()+"\", \"pos\":\""+ (quadradosPercorridos*50) +" 0\", \"group\":\"G1\" ,\"size\":\"" + (arquivos[i].Quadrados*50) + " " + (arquivos[i].Quadrados*50) + "\"}, \n";
+		quadradosPercorridos += arquivos[i].Quadrados;
+	}
+	
+	fs.appendFile('result.txt', texto, err => {
+		if (err) return console.log(err)
+	})};
 
-const escreveArquivo = function() {
-
+// gera uma cor aleatória em hexadecimal
+function gera_cor(){
+    var hexadecimais = '0123456789ABCDEF';
+    var cor = '#';
+  
+    // Pega um número aleatório no array acima
+    for (var i = 0; i < 6; i++ ) {
+    //E concatena à variável cor
+        cor += hexadecimais[Math.floor(Math.random() * 16)];
+    }
+    return cor;
 }
 
 console.log('---------------------');
@@ -104,7 +154,6 @@ console.log('Iniciando analise\n');
 
 
 inicializaPaths();
-escreveArquivo();
 
 console.log('Finalizado');
 console.log('---------------------\n');
